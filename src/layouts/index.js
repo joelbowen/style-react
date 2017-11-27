e600fad195cfabe5b4b04e38203761a9d934257e
joelbowen <joel.michael.bowen@gmail.com>
2017-11-26 22:42:54 -0500

    jss classes

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,38 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import injectSheet from 'react-jss';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
-import './index.css'
+import './index.css';
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+const headerStyles = {
+  headerContainer: {
+    backgroundColor: 'rebeccapurple',
+    marginBottom: '1.45rem',
+  },
+  header: {
+    margin: '0 auto',
+    maxWidth: 960,
+    padding: '1.45rem 1.0875rem',
+  },
+  h1: { margin: 0 },
+  link: {
+    color: 'white',
+    'text-decoration': 'none',
+    '&:hover': {
+      color: 'rgba(255, 255, 255, 0.8)',
+    },
+  },
+};
+
+/**
+ * Typically we would `export default injectSheet(headerStyles)(Header)`,
+ *   but since Gatsby put all of components together in this file, we can
+ *   use the following syntax to achieve the same HOC injection of classes.
+ */
+const Header = injectSheet(headerStyles)(({ classes }) => (
+  <div className={classes.headerContainer}>
+    <div className={classes.header}>
+      <h1 className={classes.h1}>
+        <Link to="/" className={classes.link}>
           Gatsby
         </Link>
       </h1>
     </div>
   </div>
-)
+));
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -55,10 +64,10 @@ const TemplateWrapper = ({ children }) => (
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
