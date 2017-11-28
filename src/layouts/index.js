bbb4e20716b917f57f890d2a3f8831951c018ac7
joelbowen <joel.michael.bowen@gmail.com>
2017-11-27 22:41:03 -0500

    basic styled-components use

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,36 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
 import './index.css'
 
+const HeaderContainer = styled.div`
+  background-color: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+const HeaderComponent = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+const H1 = styled.h1`
+  margin: 0;
+`
+const SiteName = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <HeaderContainer>
+    <HeaderComponent>
+      <H1>
+        <SiteName to="/">Gatsby</SiteName>
+      </H1>
+    </HeaderComponent>
+  </HeaderContainer>
 )
 
 const TemplateWrapper = ({ children }) => (
